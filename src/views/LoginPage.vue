2295051c096a4756e79fcef9dfcbfdb81060aafc
esthercf85 <esthercf85@gmail.com>
2026-08-23 23:22:44 +0200

    fix

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -112,7 +112,7 @@ const promoSlides = [
     text: 'Capions and subtitles in 25 languages',
   },
   {
-    image: 'https://adlibitumclass.com/storage/teachers/JSWEKn6ipARhXyP9wzyr2Np3xfmDPZLaJ4knNyQj.jpg"',
+    image: 'https://adlibitumclass.com/storage/teachers/JSWEKn6ipARhXyP9wzyr2Np3xfmDPZLaJ4knNyQj.jpg',
     text: 'Listen our podcasts',
   },
   {
